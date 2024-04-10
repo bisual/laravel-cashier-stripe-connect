40d7698e54d23fd81e028896fe941e27353e1bc1
Pol Ribas <polribas14@gmail.com>
2024-04-10 14:06:24 +0200

    Stripe verifiable trait

---
+++ src/Traits/StripeVerifiable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bisual\LaravelCashierStripeConnect\Traits;
+
+trait StripeVerifiable {
+    use UseStripe;
+
+    public function createVerificationSession(bool $require_matching_selfie = false) {
+        $stripe = $this->getStripeInstance();
+        return $stripe->identity->verificationSessions->create([
+            'type' => 'document',
+            'metadata' => [
+                'user_id' => $this->id,
+            ],
+            'options' => [
+                'document' => [
+                    'require_matching_selfie' => $require_matching_selfie
+                ]
+            ]
+        ]);
+    }
+
+    public function getVerificationSession($id, array $options = []) {
+        $stripe = $this->getStripeInstance();
+        return $stripe->identity->verificationSessions->retrieve($id, $options);
+    }
+}
